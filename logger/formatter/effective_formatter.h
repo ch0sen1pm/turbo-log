3a65b4ed0637700ae789693a1f8bf3808ca79d80
ch0sen1pm <18987934743@163.com>
2026-08-23 18:12:43 +0800

    feat: add effective_msg.proto and effective_formatter.h

---
+++ logger/formatter/effective_formatter.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "formatter/formatter.h"
+
+namespace logger {
+
+class EffectiveFormatter : public Formatter {
+public:
+    void Format(const LogMsg& msg, MemoryBuf* dest) override;
+};
+}
